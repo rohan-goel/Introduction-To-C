e289299670a9c48e8bd6320975c4cc03067ba4e0
Maskgirl <tithimukherjee12@gmail.com>
2020-10-05 23:18:39 +0530

    Create sorting_exercise.c

---
+++ sorting_exercise.c
@@ -0,0 +1,119 @@
+/*
+Write a programs that prints the number of pairs of elements that 
+are out-of-order for an array arr of n elements. We will call a pair 
+of elements arr[i] and arr[j] out-of-order if i < j and arr[i] > arr[j]. 
+The running time of your program should be O(n log n).
+*/
+
+/* C program for Merge Sort */
+#include <stdio.h>
+#include <stdlib.h>
+
+int count = 0;
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            /****************************************************************************************************
+                We need to count only if the value on left half is greater than then value at right half of given array
+                That's how many possible pairs can be for current element
+            *****************************************************************************************************/
+            count += n1 - i;
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void SolveByMergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        SolveByMergeSort(arr, l, m);
+        SolveByMergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+/* Driver program to test above functions */
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    SolveByMergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+
+    printf("\nnumber of pairs of elements that are out-of-order: %d\n", count);
+    return 0;
+}
